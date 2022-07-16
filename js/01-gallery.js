5afb87b2007bc787f3de4a92a3f245dad2230cbd
ArtemRe <RechembeiArtem@gmail.com>
2022-07-16 22:55:48 +0300

    1 home

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+function criateEl(array) {
+  return array.reduce(
+    (acc, item) =>
+      acc +
+      `<a class="gallery__link" 
+      href="large-image.jpg"> 
+      <img class="gallery__image"
+      src="${item.preview}" 
+      data-source="${item.original}" 
+      alt="${item.description}">
+      </img></a>`,
+    ''
+  );
+}
+criateEl(galleryItems);
+const list = document.querySelector('.gallery');
+const result = criateEl(galleryItems);
+list.insertAdjacentHTML('beforeend', result);
+
+list.addEventListener('click', listHendler);
+function listHendler(event) {
+  event.preventDefault();
+  const target = event.target;
+
+  const isGalary = target.classList.contains('gallery__image');
+  if (!isGalary) {
+    return;
+  }
+  const instance = basicLightbox.create(`<img src='${target.dataset.source}'>`);
+
+  instance.show();
+}
